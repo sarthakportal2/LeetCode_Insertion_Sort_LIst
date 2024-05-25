da25508d3bc4d33fa2677f97ab40d1e976de8f7d
sarthakportal2 <77792446+sarthakportal2@users.noreply.github.com>
2024-05-25 13:43:04 +0530

    Update Insertion_Sort_List.java

--- Insertion_Sort_List.java
+++ Insertion_Sort_List.java
@@ -1,15 +1,15 @@
 class Solution {
     public ListNode insertionSortList(ListNode head) {
-        ListNode tmp=new ListNode(0);
-        ListNode curr=head;
-        while(curr!=null){
-           ListNode prev=tmp;ListNode nextNode=curr.next;
-            while(prev.next!=null && prev.next.val<curr.val){
-                prev=prev.next;}
-                curr.next=prev.next;
-                prev.next=curr;
-                curr=nextNode;}
-        return tmp.next;}} 
+        //
+        ListNode tmp=new ListNode(0);//Temp node dynamic memory alloc
+        ListNode curr=head;//curr node declare
+        while(curr!=null){//Iterating curr node
+           ListNode prev=tmp;ListNode nextNode=curr.next;//Prev nd nextNode declare
+            while(prev.next!=null && prev.next.val<curr.val){//Iterating through entire linkedlist
+                prev=prev.next;}//Prev's current node declare 
+                curr.next=prev.next;prev.next=curr;//swapping curr nd prev's next node 
+                curr=nextNode;}//Current 's NextNode declare
+        return tmp.next;}} //Printing Inserted Sorted List 
 
     //testing
         // ListNode d=new ListNode(-1),curr=head;
